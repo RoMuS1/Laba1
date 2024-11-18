b6578b3e9bd4e4ec9f7b45564aef1eb8c183639c
RoMuS1 <153280344+RoMuS1@users.noreply.github.com>
2024-11-18 11:09:04 +0300

    remove every secend word

---
+++ 8kuyZad1.php
@@ -0,0 +1,10 @@
+function removeEveryOther($array) {
+    $newArray = [];
+    $j = 0;
+    for ($i = 0; $i < count($array); $i+=2)
+    { 
+      $newArray[$j++] = $array[$i];
+      $j++;
+    }
+  return $newArray;
+}
